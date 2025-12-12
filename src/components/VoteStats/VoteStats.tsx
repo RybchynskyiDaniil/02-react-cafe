c36b4f5502c8d24ef0cbb3871263bd5ddbb28bc8
Daniil Rybchynskyi <rybchynskyidaniil@gmail.com>
2025-12-12 20:14:47 +0000

    f

--- src/components/VoteStats/VoteStats.tsx
+++ src/components/VoteStats/VoteStats.tsx
@@ -1,20 +1,33 @@
-import type { Votes } from "../../types/votes"
-import styles from "./VoteStats.module.css"
-interface VotesStatsProps {
-    votes: Votes;
-    totalVotes: number;
-    positiveRate:number
+import type { Votes } from "../../types/votes";
+import styles from "./VoteStats.module.css";
+interface VoteStatsProps {
+  votes: Votes;
+  totalVotes: number;
+  positiveRate: number;
 }
 
-
-export default function VoteStats({votes,totalVotes = 0,positiveRate = 0 }:VotesStatsProps) {
-    return (
+export default function VoteStats({
+  votes,
+  totalVotes,
+  positiveRate,
+}: VoteStatsProps) {
+  return (
     <div className={styles.container}>
-  <p className={styles.stat}>Good: <strong>{votes.good}</strong></p>
-  <p className={styles.stat}>Neutral: <strong>{votes.neutral}</strong></p>
-  <p className={styles.stat}>Bad: <strong>{votes.bad}</strong></p>
-  <p className={styles.stat}>Total: <strong>{totalVotes}</strong></p>
-  <p className={styles.stat}>Positive: <strong>{positiveRate}%</strong></p>
-</div>
-)
+      <p className={styles.stat}>
+        Good: <strong>{votes.good}</strong>
+      </p>
+      <p className={styles.stat}>
+        Neutral: <strong>{votes.neutral}</strong>
+      </p>
+      <p className={styles.stat}>
+        Bad: <strong>{votes.bad}</strong>
+      </p>
+      <p className={styles.stat}>
+        Total: <strong>{totalVotes}</strong>
+      </p>
+      <p className={styles.stat}>
+        Positive: <strong>{positiveRate}%</strong>
+      </p>
+    </div>
+  );
 }
